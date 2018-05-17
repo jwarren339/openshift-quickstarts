b24bcd763b1f3548a5a3080aa2148d00d422c881
Jim Warren <jim-warren@sbcglobal.net>
2018-05-17 19:56:58 -0400

    Added Inetaddr

--- undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/MessageServlet.java
+++ undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/MessageServlet.java
@@ -26,6 +26,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.InetAddress;
 
 /**
  * @author Stuart Douglas
@@ -35,7 +36,9 @@ public class MessageServlet extends HttpServlet {
     public static final String MESSAGE = "message";
 
     private String message;
-
+    
+    InetAddress localhost = InetAddress.getLocalHost();
+    
     @Override
     public void init(final ServletConfig config) throws ServletException {
         super.init(config);
