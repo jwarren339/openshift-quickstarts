47dda3ffebeaa6a5f4e186de0535e4929c87d6cb
Jim Warren <jim-warren@sbcglobal.net>
2018-07-26 09:41:44 -0400

    Changed for Kevin

--- undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
+++ undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
@@ -56,7 +56,7 @@ public class ServletServer {
                     .setDeploymentName("test.war")
                     .addServlets(
                             servlet("MessageServlet", MessageServlet.class)
-                                    .addInitParam("message", "Jim Ws Java Servlet")
+                                    .addInitParam("message", "Kevins Java Servlet")
                                     .addMapping("/*"),
                             servlet("MyServlet", MessageServlet.class)
                                     .addInitParam("message", "MyServlet")
